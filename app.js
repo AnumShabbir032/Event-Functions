016ce0f28d966852ed30de4b8a596cf17aae2d32
AnumShabbir032 <AnumShabbir032@gmail.com>
2022-05-24 19:19:48 +0500

    Events Function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Section 1
+
 function changingImg(condition){
 
 var image = document.getElementById("image");
@@ -19,6 +21,8 @@ if(condition == "show"){
     }
 }
 
+// Section 2
+
 function bgColors(){
     var container = document.getElementById("container");
     var bgColorName = prompt("Enter Background Color");
@@ -34,20 +38,42 @@ function textColors(){
     para.style.color = textColor;
 
 }
+
+// Section 3
+
 function imgChange(toggle, e) {
-  console.log("helllloo", toggle, e);
-  if (toggle == "on") {
+    if (toggle == "on") {
     e.src = "./img/bulbOn1.png";
   } else {
     e.src = "./img/bulbOff1.jpg";
   }
 }
+
+// Section 4
+
 function paraColors(){
-    var para =document.getElementsByTagName("p");
-    var paraColor = prompt("Enter any color.");
+    var container1 = document.getElementById("container1")
+    var para =container1.getElementsByTagName("p");
+    // var paraColor = prompt("Enter any color.");
 
     for(var i = 0; i < para.length ; i++){
+    var paraColor = prompt(`Enter any color. ${i + 1}`);
      para[i].style.color = paraColor;
      para[i].style.fontSize = "20px"
     }
+}
+
+// Section 5
+
+function paraColorChange(){
+    var containerFive = document.getElementById("containerFive");
+    var firstAndLastPara = containerFive.getElementsByTagName('p');
+
+    var colorName = prompt("Enter color for para 1");
+    firstAndLastPara[0].style.color= colorName;
+    firstAndLastPara[0].style.fontSize = "20px"
+    
+    var colorName = prompt("Enter color for para 4");
+    firstAndLastPara[3].style.color= colorName;
+    firstAndLastPara[3].style.fontSize = "20px"
 }
